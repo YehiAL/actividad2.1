baf80dc838ec6ce165cee76f1c228ded1af83cf3
YehiAL <arayayehi01@gmail.com>
2024-10-02 16:13:30 -0300

    Acitvidad 2.1 Mandar imagen

---
+++ app/src/main/java/com/example/a21actividad/MainActivity3.java
@@ -0,0 +1,61 @@
+package com.example.a21actividad;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        imageView = findViewById(R.id.ivFoto2);
+
+        public void onClick(View v) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final Bitmap bitmap = loadImageFromNetwork("http://IP.com/image.png");
+                    imageView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+            }).start();
+        }
+
+    }
+
+    private Bitmap loadImageFromNetwork(String url) {
+        Bitmap bitmap = null;
+        try {
+            java.net.URL imageUrl = new java.net.URL(url);
+            HttpURLConnection connection = (HttpURLConnection) imageUrl.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
